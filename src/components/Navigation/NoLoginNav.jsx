c98197e388e165c26a0e4605da1beb444127990a
sahyunjin <tkguswls1106@naver.com>
2023-01-22 00:31:08 +0900

    미로그인 네비바 레이아웃 배치를 flex를 사용하여 변경함.

--- src/components/Navigation/NoLoginNav.jsx
+++ src/components/Navigation/NoLoginNav.jsx
@@ -4,30 +4,19 @@ import '../../App.css';
 import NavWrapper from "../Styled/NavWrapper";
 
 const Wrapper = styled(NavWrapper)`
-    @media(min-width: 320px) {
-        ul li {
-            list-style:none;
-            float:left;
-            padding:0px 9vw;
-            line-height:50px;
-        }  
+
+    ul {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        
+        align-items: baseline;
     }
-    @media(min-width: 425px) {
-        ul li {
-            list-style:none;
-            float:left;
-            padding:0px 13vw;
-            line-height:50px;
-        }  
-    }
-    @media(min-width: 768px) {
-        ul li {
-            list-style:none;
-            float:left;
-            padding:0px 18vw;
-            line-height:50px;
-        }  
-    }
+
+    ul li {
+        list-style:none;
+        line-height:50px;
+    }  
 
     a {
         text-decoration:none;
