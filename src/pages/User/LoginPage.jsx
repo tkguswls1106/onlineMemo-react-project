2170549f5e60f7124bdd139b911e6e4d0bc631be
sahyunjin <tkguswls1106@naver.com>
2023-06-30 22:28:54 +0900

    에러 해결 실험중.

--- src/pages/User/LoginPage.jsx
+++ src/pages/User/LoginPage.jsx
@@ -66,7 +66,7 @@ function LoginPage(props) {
         // e.preventDefault();  // 리프레쉬 방지 (spa로서)
 
         await axios
-            .post('/login', {
+            .post('https://www.onlinememo.kr/login', {
                 loginId: loginIdValue,
                 firstPw: pwValue
             })
@@ -87,7 +87,7 @@ function LoginPage(props) {
 
     async function checkLogin() {  // 로그인 상태 여부 확인하고 해당 사용자의 userId 반환
         await axios
-            .get('/auth')
+            .get('https://www.onlinememo.kr/auth')
             .then((response) => {
                 setTokenUserId(response.data.data.id);
                 //console.log(response);
